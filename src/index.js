4eb0e3b04d42fe4b532400d4e59b0278dd6ab626
Diogo <diogoslva0133@gmail.com>
2022-10-27 10:53:31 -0300

    creation project

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './index.css'
+import ReactDom from 'react-dom'
+import React from 'react'
+
+ const tag =  <strong>Olá React!</strong>
+
+ReactDom.render(
+    <div>
+        {tag}
+    </div>, 
+    document.getElementById('root')
+)
