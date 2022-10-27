47aa5c37de2c297853faa0a5eddeb65b169750de
Diogo <diogoslva0133@gmail.com>
2022-10-27 12:17:02 -0300

    add : React Fragment

---
+++ src/components/components/Fragmento.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Fragmento(props) {
+    return ( //quando quiser inserir algum atrivbuto amais... usat React Fragment
+        <> 
+            <h2>Fragemento</h2>
+            <p>Cuidado com esse erro!</p>
+        </>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ import React from 'react'
 
 import Primeiro from './components/components/Primeiro'
 import Comparametro from './components/components/ComParametro'
+import Fragmento from './components/components/Fragmento'
+
 
 ReactDom.render(
     <div id='app'>
         <Primeiro></Primeiro>
         <Comparametro 
-        titulo="Situacao do aluno" 
-        aluno='Pedro!' nota={9.3}/> 
-        
+            titulo="Situacao do aluno" 
+            aluno='Pedro!' 
+            nota={8.3}/> 
+        <Fragmento/>
+
     </div>,//podemos usar o fechamento de duas formas com o parametro ou apenas o /> 
     document.getElementById('root')
 )
