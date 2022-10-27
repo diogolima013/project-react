275e798689cf8d63294a31de7690a797dbfbe539
Diogo <diogoslva0133@gmail.com>
2022-10-27 11:59:44 -0300

    component with property

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDom from 'react-dom'
 import React from 'react'
 
 import Primeiro from './components/components/Primeiro'
+import Comparametro from './components/components/ComParametro'
 
 ReactDom.render(
-    <div>
+    <div id='app'>
         <Primeiro></Primeiro>
-    </div>, 
+        <Comparametro 
+        titulo="Situacao do aluno" 
+        aluno='Pedro!' nota={9.3}/> 
+        
+    </div>,//podemos usar o fechamento de duas formas com o parametro ou apenas o /> 
     document.getElementById('root')
 )
