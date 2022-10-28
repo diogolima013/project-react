becf6ba7d08078854b201df6497db5e5c21a0956
Diogo <diogoslva0133@gmail.com>
2022-10-28 09:32:26 -0300

    add : Component Card #03

--- src/App.jsx
+++ src/App.jsx
@@ -13,15 +13,15 @@ export default () => (
         <h1>Fundamentos React </h1>
 
         <div className="Cards">
-            <Card titulo='#04 - Desafio Aleatório'>
+            <Card titulo='#04 - Desafio Aleatório' color='#FA6900'>
                 <Aleatorio min={1} max={60}/>
             </Card>
 
-            <Card titulo='#03 - Fragmento'>
+            <Card titulo='#03 - Fragmento'  color='#E94C6F'>
                 <Fragmento/>
             </Card>
 
-            <Card titulo='#02 - Com Parâmetro'>
+            <Card titulo='#02 - Com Parâmetro'  color='#E8B71A'>
                 <Comparametro 
                     titulo="Situação do aluno" 
                     aluno='Pedro Silva' 
@@ -29,7 +29,7 @@ export default () => (
                 /> 
             </Card>
 
-            <Card titulo='#01 - Primeiro Componente'>
+            <Card titulo='#01 - Primeiro Componente'  color='#588C73'>
                 <Primeiro></Primeiro>
             </Card>
 
